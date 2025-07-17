b836fa7d7771e8c8999a96e06b641a40f4e5f562
404Shihab <shihabbhuiyan500@gmail.com>
2025-07-17 20:28:04 +0600

    updated

--- Basic/while_loop.cpp
+++ Basic/while_loop.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main() 
 {
     //Initialization: Set the starting value of the loop variable
-    int i = 1;
+    int i=1;
 
     //Condition: The loop will continue as long as this condition is true
-    while (i <= 5) {
-        cout << i << endl;
+    while (i<=5) 
+    {
+        cout <<i<< endl;
 
         //Update: Change the loop variable to avoid infinite loop
         i++;
